721253b0004dfe73af48179def20699d3b30a25b
David K <davidknight11207@gmail.com>
2026-02-04 22:15:24 -0500

    Filehandler implementation, data files, and first tests
    Also spent about 2 hours figuring out JUnit

---
+++ src/main/java/FileHandler.java
@@ -0,0 +1,68 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public class FileHandler {
+    String readFileName;
+    ArrayList<String> storedFiles;
+
+    String docFolder = "data"; //directory for base text file
+
+    public FileHandler(String[] args){ //if commandline arguments are passed along
+        storedFiles = new ArrayList<String>();
+        for(int i = 0; i < args.length; i++){
+            Path path = Paths.get(args[i]);
+            if(Files.exists(path)){
+                storedFiles.add(args[i]);
+                System.out.println("File path added to list successfully.");
+            }
+        }
+    }
+
+    public FileHandler(){ //blank constructor without a commandline argument
+
+    }
+
+    public String readDefaultFiles(){
+        System.out.println("Default Files:");
+        System.out.println("~~~~~~~~~~~~~~");
+
+        Path dataPath = Paths.get(System.getProperty("user.dir"), docFolder); //appends the root directory with the docFolder directory
+        System.out.println("Path: " + dataPath.toString());
+
+        System.out.println("Files:");
+
+        try(Stream<Path> stream = Files.list(dataPath)){
+            stream.map(Path::getFileName).forEach(System.out::println);
+        } catch(IOException e){
+            e.printStackTrace(); //in the event of an IOException
+        }
+
+        return dataPath.toString();
+    }
+
+    public void readFile(String path){
+        Path inputPath = Paths.get(path);
+        if(Files.exists(inputPath)){
+            try{ //inside a try block, as IOExceptions will terminate program control if not caught and handled properly
+                List<String> lines = Files.readAllLines(Paths.get(path));
+                for(int i = 0; i < lines.size(); i++){
+                    System.out.println(lines.get(i));
+                }
+            }
+            catch(IOException e){
+                //what to do if the IOException is caught
+            }
+            finally{
+
+            }
+        }
+        else{
+            System.out.println("File from path " + path + " does not exist");
+        }
+    }
+}
--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -2,4 +2,12 @@
  * Commmand Line Utility
  */
 public class TopSecret {
+    public static void main(String[] args){
+        System.out.println("");
+        System.out.println("Welcome to Top Secret Cipher Software - Command Line Edition");
+        System.out.println("");
+
+        FileHandler handler = new FileHandler(args);
+        handler.readDefaultFiles();
+    }
 }
