38d1ba1803e87c2ae39bc0bf45564ed50ae36fda
Mehul <hgp5ka@virginia.edu>
2026-02-12 15:01:53 -0500

    Team Member A first commit-- test

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -7,12 +7,55 @@ public class TopSecret {
         System.out.println("Welcome to Top Secret Cipher Software - Command Line Edition");
         System.out.println("");
 
-        FileHandler handler = new FileHandler(args);
-        handler.readDefaultFiles();
-        handler.readFileFromDataFolder("test2.txt");
+        ProgramControl control = new ProgramControl();
+        if(args.length == 0){
+            control.printFileList();
+            return;
+        }
+
+        if (args.length == 1){
+            String arg = args[0];
+            int fileNum;
+
+            try {
+                fileNum = Integer.parseInt(arg);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: file number must be numeric.");
+                return;
+            }
+
+            String content = control.getFileContentsByNumber(fileNum);
+            System.out.println(content);
+        }
+
+        if (args.length == 2){
+            String arg = args[0];
+            String keyFile = args[1];
+            int fileNum;
+
+            try {
+                fileNum = Integer.parseInt(arg);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: file number must be numeric.");
+                return;
+            }
+
+            String content = control.getFileContentsByNumber(fileNum);
+
+            //placeholder for D
+            System.out.println("Using key: " + keyFile + "\n");
+            System.out.println(content);
+            return;
+        }
+
+        System.out.println("Too many arguments.");
+
+        // FileHandler handler = new FileHandler(args);
+        //handler.readDefaultFiles();
+        //handler.readFileFromDataFolder("test2.txt");
 
         //System.out.println("Reading char index from file:");
-        char returned = handler.readFileIndexFromData("test2.txt", 7);
-        System.out.println(returned);
+        // char returned = handler.readFileIndexFromData("test2.txt", 7);
+        // System.out.println(returned);
     }
 }
